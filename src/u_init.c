d848fa32301c271d8de0bc785fd1678902e9f4a7
Ron Nazarov <ron@noisytoot.org>
2024-10-10 19:39:56 +0100

    Make all gnomes start with medium hats and allow wearing them
    Including the hat in the mad stuff box.
    
    Any hat can now be worn even if it's one size too large (huge ->
    gigantic counts as 3 sizes for the purposes of this calculation).

--- src/u_init.c
+++ src/u_init.c
@@ -3173,6 +3173,10 @@ ini_inv(register struct trobj *trop)
 			if(obj->otyp == FACELESS_HELM && Role_if(PM_MADMAN)){
 				set_material_gm(obj, IRON);
 			}
+			/* Gnomish pointy hats are supposed to be medium */
+			if(obj->otyp == GNOMISH_POINTY_HAT && Race_if(PM_GNOME)){
+				obj->objsize = MZ_MEDIUM;
+			}
 			
 			/* Don't start with +0 or negative rings */
 			if (objects[obj->otyp].oc_charged && obj->spe <= 0)
